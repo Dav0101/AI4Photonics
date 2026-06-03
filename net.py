973e6a568ffa9bb2053979401d1416dc021943a4
Davide Cappa <davide.cappa01@gmail.com>
2026-06-03 22:01:24 +0200

    creation of rcwa solver class (copied from surface.py) and insert it in net.py

--- net.py
+++ net.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn as nn
 from torch import Tensor
+import torcwa
+import numpy as np
 
 
 def gumbel_sigmoid(logits: Tensor, tau: float = 1, hard: bool = False, threshold: float = 0.5) -> Tensor:
@@ -26,6 +28,58 @@ def rcwa(rho : Tensor, angle : Tensor) -> Tensor:
     return torch.tensor([[1+2j, 3-1j], [4+0j, 5+2j]], dtype=torch.complex64, device=rho.device)
 
 
+class rcwa_solver():
+    def __init__(self, device):
+        self.device=device
+        # Simulation environment
+        # light
+        self.sim_dtype = torch.complex64
+        geo_dtype = torch.float32
+        self.lamb0 = torch.tensor(600.,dtype=geo_dtype,device=device)    # nm
+        self.theta = 10.01*(np.pi/180)    # radian
+
+        # material
+        self.substrate_eps = 1.46**2
+        #silicon_eps = Materials.aSiH.apply(lamb0)**2
+        self.silicon_eps = 3.5**2
+
+        # geometry
+        self.L = [2048., 1024.]            # nm / nm
+        torcwa.rcwa_geo.dtype = geo_dtype
+        torcwa.rcwa_geo.device = device
+        torcwa.rcwa_geo.Lx = self.L[0]
+        torcwa.rcwa_geo.Ly = self.L[1]
+        torcwa.rcwa_geo.nx = 256
+        torcwa.rcwa_geo.ny = 128
+        torcwa.rcwa_geo.grid()
+        torcwa.rcwa_geo.edge_sharpness = 1000.
+
+        # layers
+        self.layer0_thickness = 300.
+
+    # this returns the scattering matrix for the chosen deflection and incident orders
+    def solve(self, rho, phi):
+        # radius for m and n respectively
+        order = [10,4]
+
+        sim = torcwa.rcwa(freq=1/self.lamb0,order=order,L=self.L,dtype=self.sim_dtype,device=self.device)
+        sim.add_input_layer(eps=self.substrate_eps)
+        sim.set_incident_angle(inc_ang=self.theta,azi_ang=phi)
+        epsilon = rho*self.silicon_eps + (1.-rho)
+        sim.add_layer(thickness=self.layer0_thickness,eps=epsilon)
+
+        sim.solve_global_smatrix()
+        S_ss = sim.S_parameters(orders=[1,0],direction='forward',port='transmission',polarization='ss',ref_order=[0,0])
+        print(S_ss)
+        S_pp = sim.S_parameters(orders=[1,0],direction='forward',port='transmission',polarization='pp',ref_order=[0,0])
+        S_sp = sim.S_parameters(orders=[1,0],direction='forward',port='transmission',polarization='sp',ref_order=[0,0])
+        S_ps = sim.S_parameters(orders=[1,0],direction='forward',port='transmission',polarization='ps',ref_order=[0,0])
+
+        row1 = torch.stack([S_ss, S_ps])
+        row2 = torch.stack([S_sp, S_pp])
+        return torch.stack([row1, row2])
+
+
 class ConvNetRCWA(nn.Module):
     def __init__(self, n, m, step):
         super().__init__()
@@ -47,12 +101,12 @@ class ConvNetRCWA(nn.Module):
             nn.Conv2d(32, 1, kernel_size=3, padding=1)
         )
 
-    def forward(self):
+    def forward(self, solver):
         rho = gumbel_sigmoid(self.net(self.initial_rho), hard=True)
         sigmas = []
 
         for angle in self.angles:
-            jones_matrix = rcwa(rho, angle)
+            jones_matrix = solver.solve(rho, angle)
             singular_values = torch.linalg.svdvals(jones_matrix)
             sigmas.append(torch.min(singular_values))
 
@@ -63,8 +117,9 @@ if __name__ == '__main__':
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = ConvNetRCWA(n=2, m=3, step=36)
     model = model.to(device)
+    solver = rcwa_solver(device)
 
-    r, v = model()
+    r, v = model(solver)
 
     print(r.shape)
     print(v)
